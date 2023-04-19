ed99c516004fe3ebabb8e5f2dd293e1048cd8fa8
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-04-19 15:26:04 +0200

    complete ChatGPTServlet

--- composum/bundle/src/main/java/com/composum/chatgpt/bundle/ChatGPTServlet.java
+++ composum/bundle/src/main/java/com/composum/chatgpt/bundle/ChatGPTServlet.java
@@ -55,7 +55,7 @@ public class ChatGPTServlet extends AbstractServiceServlet {
 
     public enum Extension {json}
 
-    public enum Operation {hello, translate}
+    public enum Operation {translate, keywords, description, prompt}
 
     protected final ServletOperationSet<Extension, Operation> operations = new ServletOperationSet<>(Extension.json);
 
@@ -71,6 +71,12 @@ public class ChatGPTServlet extends AbstractServiceServlet {
         for (ServletOperationSet.Method method : List.of(ServletOperationSet.Method.GET, ServletOperationSet.Method.POST)) {
             operations.setOperation(method, Extension.json, Operation.translate,
                     new TranslateOperation());
+            operations.setOperation(method, Extension.json, Operation.keywords,
+                    new KeywordsOperation());
+            operations.setOperation(method, Extension.json, Operation.description,
+                    new DescriptionOperation());
+            operations.setOperation(method, Extension.json, Operation.prompt,
+                    new PromptOperation());
         }
     }
 
@@ -98,26 +104,114 @@ public class ChatGPTServlet extends AbstractServiceServlet {
 
         protected abstract void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response);
 
+        protected Integer getOptionalInt(Status status, SlingHttpServletRequest request, String parameterName) {
+            String parameter = request.getParameter(parameterName);
+            if (parameter != null) {
+                try {
+                    return Integer.parseInt(parameter);
+                } catch (NumberFormatException e) {
+                    status.error("Invalid nonnumeric value for parameter " + parameterName + ": " + parameter);
+                }
+            }
+            return null;
+        }
     }
 
     /**
      * Servlet representation of {@link GPTTranslationService}, specifically {@link GPTTranslationService#singleTranslation(String, String, String)}
-     * with argument text, sourceLanguage, targetLanguage .
-     * Input are the parameters text, sourceLanguage, targetLanguage, output is a list "translation" containing the translation as (currently) a single string.
-     * We use a list since it might be sensible to create multiple translation variants in the future.
+     * with arguments text, sourceLanguage, targetLanguage .
+     * Input are the parameters text, sourceLanguage, targetLanguage, output is in data.result.translation
+     * a list containing the translation as (currently) a single string.
+     * We use a list since it might be sensible to create multiple translation variants in the future, if requested.
      */
     // http://localhost:9090/bin/cpm/platform/chatgpt/servlet.translate.json?sourceLanguage=en&targetLanguage=de&text=Hello%20World
     public class TranslateOperation extends AbstractGPTServletOperation {
 
         @Override
         protected void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response) {
-            // reimplement old doIt here
             String text = status.getRequiredParameter("text", null, "No text to translate");
             String sourceLanguage = status.getRequiredParameter("sourceLanguage", null, "No source language");
             String targetLanguage = status.getRequiredParameter("targetLanguage", null, "No target language");
             if (status.isValid()) {
                 String translation = translationService.singleTranslation(text, sourceLanguage, targetLanguage);
-                status.data("result").put("translation", translation);
+                status.data("result").put("translation", List.of(translation));
+            }
+        }
+
+    }
+
+    /**
+     * Servlet representation of {@link GPTContentCreationService#generateKeywords(String)} with argument text.
+     * Input parameters is text, output is in data.result.keywords a list of keywords.
+     */
+    public class KeywordsOperation extends AbstractGPTServletOperation {
+
+        @Override
+        protected void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response) {
+            String text = status.getRequiredParameter("text", null, "No text given");
+            if (status.isValid()) {
+                List<String> result = contentCreationService.generateKeywords(text);
+                status.data("result").put("keywords", result);
+            }
+        }
+
+    }
+
+    /**
+     * Servlet representation of {@link GPTContentCreationService#generateDescription(String, int)} with arguments text and maxwords.
+     * Input parameters is text and the optional numeric parameter maxwords,
+     * output is in data.result.description a string containing the description.
+     */
+    public class DescriptionOperation extends AbstractGPTServletOperation {
+
+        @Override
+        protected void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response) {
+            String text = status.getRequiredParameter("text", null, "No text given");
+            Integer maxwords = getOptionalInt(status, request, "maxwords");
+            if (status.isValid()) {
+                String result = contentCreationService.generateDescription(text, maxwords != null ? maxwords : -1);
+                status.data("result").put("description", result);
+            }
+        }
+
+    }
+
+    /**
+     * Servlet representation of {@link com.composum.chatgpt.base.service.chat.GPTContentCreationService#executePrompt(java.lang.String, int)}
+     * with arguments prompt and maxwords.
+     * Input parameters is text and the optional numeric parameter maxwords,
+     * output is in data.result.text the generated text.
+     */
+    public class PromptOperation extends AbstractGPTServletOperation {
+
+        @Override
+        protected void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response) {
+            String prompt = status.getRequiredParameter("prompt", null, "No prompt given");
+            Integer maxwords = getOptionalInt(status, request, "maxwords");
+            if (status.isValid()) {
+                String result = contentCreationService.executePrompt(prompt, maxwords != null ? maxwords : -1);
+                status.data("result").put("text", result);
+            }
+        }
+
+    }
+
+    /**
+     * Servlet representation of {@link com.composum.chatgpt.base.service.chat.GPTContentCreationService#executePromptOnText(java.lang.String, java.lang.String, int)}
+     * with arguments prompt, text and maxwords.
+     * Input parameters is a text, the prompt to execute on it and the optional numeric parameter maxwords,
+     * output is in data.result.text the generated text.
+     */
+    public class PromptOnTextOperation extends AbstractGPTServletOperation {
+
+        @Override
+        protected void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response) {
+            String prompt = status.getRequiredParameter("prompt", null, "No prompt given");
+            String text = status.getRequiredParameter("text", null, "No text given");
+            Integer maxwords = getOptionalInt(status, request, "maxwords");
+            if (status.isValid()) {
+                String result = contentCreationService.executePromptOnText(prompt, text, maxwords != null ? maxwords : -1);
+                status.data("result").put("text", result);
             }
         }
 
