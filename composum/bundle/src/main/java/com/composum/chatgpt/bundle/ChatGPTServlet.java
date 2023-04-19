d1f1af9b6bac6bbac6a18d7e401a7ec0ad2678a6
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-04-19 15:04:19 +0200

    refactoring of servlet to ease adding other operations

--- composum/bundle/src/main/java/com/composum/chatgpt/bundle/ChatGPTServlet.java
+++ composum/bundle/src/main/java/com/composum/chatgpt/bundle/ChatGPTServlet.java
@@ -1,10 +1,11 @@
 package com.composum.chatgpt.bundle;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.jcr.RepositoryException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
@@ -21,9 +22,7 @@ import org.slf4j.LoggerFactory;
 
 import com.composum.chatgpt.base.service.GPTException;
 import com.composum.chatgpt.base.service.chat.GPTChatCompletionService;
-import com.composum.chatgpt.base.service.chat.GPTChatRequest;
 import com.composum.chatgpt.base.service.chat.GPTContentCreationService;
-import com.composum.chatgpt.base.service.chat.GPTMessageRole;
 import com.composum.chatgpt.base.service.chat.GPTTranslationService;
 import com.composum.sling.core.ResourceHandle;
 import com.composum.sling.core.servlet.AbstractServiceServlet;
@@ -70,11 +69,35 @@ public class ChatGPTServlet extends AbstractServiceServlet {
         super.init();
         // FIXME(hps,19.04.23) only use POST later, but for now, GET is easier to test
         for (ServletOperationSet.Method method : List.of(ServletOperationSet.Method.GET, ServletOperationSet.Method.POST)) {
-            operations.setOperation(method, Extension.json, Operation.hello,
-                    new HelloOperation());
             operations.setOperation(method, Extension.json, Operation.translate,
                     new TranslateOperation());
         }
+    }
+
+    protected abstract class AbstractGPTServletOperation implements ServletOperation {
+
+        /**
+         * Frame implementation with common code
+         */
+        @Override
+        public final void doIt(@Nonnull SlingHttpServletRequest request, @Nonnull SlingHttpServletResponse response, @Nullable ResourceHandle resource) throws RepositoryException, IOException, ServletException {
+            Status status = new Status(request, response, LOG);
+
+            try {
+                performOperation(status, request, response);
+            } catch (GPTException e) {
+                status.error("Error accessing ChatGPT", e);
+                status.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            } catch (RuntimeException e) {
+                status.error("Internal error", e);
+                status.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+
+            status.sendJson();
+        }
+
+        protected abstract void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response);
+
     }
 
     /**
@@ -84,47 +107,20 @@ public class ChatGPTServlet extends AbstractServiceServlet {
      * We use a list since it might be sensible to create multiple translation variants in the future.
      */
     // http://localhost:9090/bin/cpm/platform/chatgpt/servlet.translate.json?sourceLanguage=en&targetLanguage=de&text=Hello%20World
-    public class TranslateOperation implements ServletOperation {
-        @Override
-        public void doIt(@Nonnull SlingHttpServletRequest request, @Nonnull SlingHttpServletResponse response, @Nonnull ResourceHandle resource) throws IOException {
-            Status status = new Status(request, response, LOG);
+    public class TranslateOperation extends AbstractGPTServletOperation {
 
+        @Override
+        protected void performOperation(Status status, SlingHttpServletRequest request, SlingHttpServletResponse response) {
+            // reimplement old doIt here
             String text = status.getRequiredParameter("text", null, "No text to translate");
             String sourceLanguage = status.getRequiredParameter("sourceLanguage", null, "No source language");
             String targetLanguage = status.getRequiredParameter("targetLanguage", null, "No target language");
-
-            try {
+            if (status.isValid()) {
                 String translation = translationService.singleTranslation(text, sourceLanguage, targetLanguage);
                 status.data("result").put("translation", translation);
-            } catch (GPTException e) {
-                status.error("Error accessing ChatGPT", e);
-                status.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            }
-
-            status.sendJson();
-        }
-
-    }
-
-    /**
-     * A quick hello world, http://localhost:9090/bin/cpm/platform/chatgpt/servlet.hello.json
-     */
-    // FIXME(hps,19.04.23) remove later
-    public class HelloOperation implements ServletOperation {
-
-        @Override
-        public void doIt(@Nonnull SlingHttpServletRequest request, @Nonnull SlingHttpServletResponse response, @Nonnull ResourceHandle resource) throws IOException, ServletException {
-            response.setContentType("text/plain");
-            try (PrintWriter writer = response.getWriter()) {
-                writer.write("Hello World!");
-                writer.write("Trying to access ChatGPT...");
-                writer.flush();
-                GPTChatRequest chatrequest = new GPTChatRequest();
-                chatrequest.addMessage(GPTMessageRole.USER, "Hi!");
-                String chatresponse = chatService.getSingleChatCompletion(chatrequest);
-                writer.write("ChatGPT says: " + chatresponse);
             }
         }
+
     }
 
 }
