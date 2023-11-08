2c4a1a202896b9407b04f6502d2c24946b06dbee
Ralf Wunsch <r.wunsch@ist-software.com>
2023-11-08 14:36:57 +0000

    Deploying to gh-pages from @ ist-dresden/composum-AI@9294eb5d771c9b4727fb25a1c61632bac0a322d9 🚀

---
+++ composum-ai-integration-backend/composum-ai-integration-backend-slingbase/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.composum.ai.backend.slingbase"},{"l":"com.composum.ai.backend.slingbase.impl"},{"l":"com.composum.ai.backend.slingbase.model"}]
--- composum-ai-integration-composum/composum-ai-integration-composum-bundle/apidocs/package-search-index.js
+++ composum-ai-integration-composum/composum-ai-integration-composum-bundle/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.composum.ai.composum.bundle"},{"l":"com.composum.ai.composum.bundle.model"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.composum.ai.composum.bundle"},{"l":"com.composum.ai.composum.bundle.model"},{"l":"com.composum.ai.composum.bundle.service"}]
