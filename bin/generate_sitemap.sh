35a0575794f151796b395a611c3a439ef5fc2590
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-11-10 14:14:55 +0100

    automatically create a sitemap

---
+++ bin/generate_sitemap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SITEMAP="target/site/sitemap.xml"
+mkdir -p target/site
+
+# Start the sitemap file
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $SITEMAP
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $SITEMAP
+
+# For each .md file in the repository
+for file in $(find src/site/markdown -type f -name "*.md"); do
+    # Get the last commit date for the file
+    last_commit_date=$(git log -1 --format=%cd --date=format:'%Y-%m-%d' -- "$file")
+    
+    # Convert the .md file path to .html
+    html_path="${file%.md}.html"
+
+    echo $html_path
+
+    # Remove the leading src/site/markdown/
+    html_path="${html_path#src/site/markdown/}"
+
+    # Append the entry to the $SITEMAP
+echo "  <url>" >> $SITEMAP
+echo "    <loc>https://ist-dresden.github.io/composum-AI/$html_path</loc>" >> $SITEMAP
+echo "    <lastmod>$last_commit_date</lastmod>" >> $SITEMAP
+echo "  </url>" >> $SITEMAP
+done
+
+# Close the $SITEMAP file
+echo '</urlset>' >> $SITEMAP
