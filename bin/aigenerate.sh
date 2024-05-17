bbc5454c180b32eb125dba95c5c27a22d9778516
Dr. Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2024-05-17 18:34:34 +0200

    starting templating GUI

--- bin/aigenerate.sh
+++ bin/aigenerate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
-aigenpipeline -dd -os 'backend/slingbase/**/main/**/*'
-aigenpipeline -os 'backend/slingbase/**/main/**/*'
+aigenpipeline -dd -os '**/src/**/*'
+aigenpipeline -os '**/src/**/*'
