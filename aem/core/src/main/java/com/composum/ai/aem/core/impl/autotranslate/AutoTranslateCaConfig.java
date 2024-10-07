2aa38077be8aa4128a7f5cdf4248946c588c79fb
Dr. Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2024-10-07 07:49:59 +0200

    Set order and textarea properties in auto translate ca configuration

--- aem/core/src/main/java/com/composum/ai/aem/core/impl/autotranslate/AutoTranslateCaConfig.java
+++ aem/core/src/main/java/com/composum/ai/aem/core/impl/autotranslate/AutoTranslateCaConfig.java
@@ -9,29 +9,46 @@ import org.apache.sling.caconfig.annotation.Property;
 // is also added to Sling-ContextAware-Configuration-Classes bnd header in pom.xml
 public @interface AutoTranslateCaConfig {
 
-    @Property(label = "Additional Instructions", description = "Additional instructions for the automatic translation.")
+    @Property(label = "Additional Instructions", order = 1,
+            description = "Additional instructions for the automatic translation.",
+            property = {
+                    "widgetType=textarea",
+                    "textareaRows=8"
+            })
     String additionalInstructions();
 
-    @Property(label = "Prefer High Intelligence Model", description = "If set, the high intelligence model will be used for translation.")
-    boolean preferHighIntelligenceModel();
-
-    @Property(label = "Prefer Standard Model", description = "If set, the standard model will be used for translation. Opposite of 'Prefer High Intelligence Model'.")
-    boolean preferStandardModel();
-
-    @Property(label = "Rules that give additional instructions for translation if certain words or phrases are present in the page.")
+    @Property(label = "Rules for additional Instructions", order = 2,
+            description = "Rules that give additional instructions for translation if certain words or phrases are present in the page.")
     AutoTranslateRuleConfig[] rules() default {};
 
-    @Property(label = "Include Full Page during Retranslation",
+    @Property(label = "Prefer High Intelligence Model", order = 3,
+            description = "If set, the high intelligence model will be used for translation.")
+    boolean preferHighIntelligenceModel();
+
+    @Property(label = "Prefer Standard Model", order = 4,
+            description = "If set, the standard model will be used for translation. Opposite of 'Prefer High Intelligence Model'.")
+    boolean preferStandardModel();
+
+    @Property(label = "Include Full Page during Retranslation", order = 5,
             description = "If true we do not only provide changed texts to the AI during re-translating a page with some changes," +
                     "but give the entire page to provide better context. That is a bit slower and a bit more expensive, but likely" +
                     "improves the result. This overrides the default from OSGI configuration.")
     boolean[] includeFullPageInRetranslation();
 
-    @Property(label = "Include Existing Translations in Retranslation",
+    @Property(label = "Include Existing Translations in Retranslation", order = 6,
             description = "If true, when retranslating a page with some changes we provide" +
                     "the existing translations of that page to the AI as well as additional context with examples. " +
                     "That is a bit slower and a bit more expensive, but likely improves the result." +
                     "This overrides the default from OSGI configuration.")
     boolean[] includeExistingTranslationsInRetranslation() default true;
 
+    @Property(label = "Dropdown Param", description = "Parameter with dropdown list.", property = {
+            "widgetType=dropdown",
+            "dropdownOptions=["
+                    + "{'value':'','description':'First option'},"
+                    + "{'value':'true','description':'Second option'},"
+                    + "{'value':'false','description':'Third option'}"
+                    + "]"
+    })
+    String dropdownParam();
 }
--- aem/core/src/main/java/com/composum/ai/aem/core/impl/autotranslate/AutoTranslateRuleConfig.java
+++ aem/core/src/main/java/com/composum/ai/aem/core/impl/autotranslate/AutoTranslateRuleConfig.java
@@ -9,18 +9,25 @@ import org.apache.sling.caconfig.annotation.Property;
  */
 public @interface AutoTranslateRuleConfig {
 
-    @Property(label = "Path Regex", description = "A regular expression matching the absolute path to the page. " +
-            "E.g. .*/home/products/.* will match all pages under .../home/products/. If empty every page will match" +
-            "if the content pattern condition is met.", order = 1)
+    @Property(label = "Path Regex", order = 1,
+            description = "A regular expression matching the absolute path to the page. " +
+                    "E.g. .*/home/products/.* will match all pages under .../home/products/. If empty every page will match" +
+                    "if the content pattern condition is met.")
     String pathRegex();
 
-    @Property(label = "Content Pattern", description = "A word or phrase that must be present in the content of the page for the rule to match. " +
-            "E.g. 'Product' will match all pages that contain the word 'Product', case-insensitive. Spaces will also match any whitespace. " +
-            "If it contains any of the regex meta characters []|()*+ it'll be treated as a regular expression." +
-            "If empty every page will match if the path condition is met.", order = 2)
+    @Property(label = "Content Pattern", order = 2,
+            description = "A word or phrase that must be present in the content of the page for the rule to match. " +
+                    "E.g. 'Product' will match all pages that contain the word 'Product', case-insensitive. Spaces will also match any whitespace. " +
+                    "If it contains any of the regex meta characters []|()*+ it'll be treated as a regular expression." +
+                    "If empty every page will match if the path condition is met.")
     String contentPattern();
 
-    @Property(label = "Additional Instructions", description = "Additional instructions for the automatic translation in case this rule matches.", order = 3)
+    @Property(label = "Additional Instructions", order = 3,
+            description = "Additional instructions for the automatic translation in case this rule matches.",
+            property = {
+                    "widgetType=textarea",
+                    "textareaRows=5"
+            })
     String additionalInstructions();
 
 }
