cfc41878ea6619723d0719ee216c93341fd3d09a
Dr. Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2024-12-16 15:09:32 +0100

    parse csv files

--- aem/core/src/main/java/com/composum/ai/aem/core/impl/autotranslate/TranslationRuleExtractor.java
+++ aem/core/src/main/java/com/composum/ai/aem/core/impl/autotranslate/TranslationRuleExtractor.java
@@ -2,57 +2,127 @@ package com.composum.ai.aem.core.impl.autotranslate;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Stream;
+import java.util.zip.ZipException;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.sling.api.resource.Resource;
+import org.dhatim.fastexcel.reader.ReadableWorkbook;
+import org.dhatim.fastexcel.reader.Row;
+import org.dhatim.fastexcel.reader.Sheet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.Rendition;
+import com.day.text.csv.Csv;
 
 /**
- * Extracts translation rules from excel documents.
+ * Reads translation rules from an xls document.
+ * <p>
+ * It's difficult to use POI since the AEM uber-jar 6.5.7 which we include does not contain the full POI library
+ * and cannot read XLSX files. We use the fastexcel library instead.
+ * </p>
  */
 public class TranslationRuleExtractor {
 
     private static final Logger LOG = LoggerFactory.getLogger(TranslationRuleExtractor.class);
 
-    /**
-     * Collects a mapping from key to value from the given excel resource, starting from the given row, and using the given columns for key and value.
-     */
-    public Map<String, String> extractRules(Resource xlsResource, int sheetIndex, int startRow, int keyColumn, int valueColumn) throws IOException {
-        Map<String, String> rules = new LinkedHashMap<>();
-        try (InputStream inputStream = getAssetInputStream(xlsResource)) {
-            Workbook workbook = WorkbookFactory.create(inputStream);
-            Sheet sheet = workbook.getSheetAt(sheetIndex);
-            for (int i = startRow; i <= sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-                String key = row.getCell(keyColumn).getStringCellValue();
-                String value = row.getCell(valueColumn).getStringCellValue();
-                if (key != null && !key.isEmpty() && value != null && !value.isEmpty()) {
-                    rules.put(key, value);
-                }
-            }
-
+    public Map<String, String> extractRules(Resource resource, int sheetIndexBase1, int startRowBase1,
+                                            String keyColumnStr, String valueColumnStr) throws IOException {
+        if (resource == null) {
+            throw new IllegalArgumentException("Could not find resource.");
         }
-        LOG.info("Extracted {} rules from {}", rules.size(), xlsResource.getPath());
+        if (sheetIndexBase1 < 1) {
+            throw new IllegalArgumentException("Sheet index must be at least 1.");
+        }
+        if (startRowBase1 < 1) {
+            throw new IllegalArgumentException("Start row must be at least 1.");
+        }
+        int keyColumn = keyColumnStr.toUpperCase().charAt(0) - 'A';
+        int valueColumn = valueColumnStr.toUpperCase().charAt(0) - 'A';
+        if (resource.getPath().contains(".csv")) {
+            return extractRulesFromCsv(resource, sheetIndexBase1 - 1, startRowBase1 - 1, keyColumn, valueColumn);
+        } else {
+            return extractRulesFromXlsx(resource, sheetIndexBase1 - 1, startRowBase1 - 1, keyColumn, valueColumn);
+        }
+    }
+
+    /**
+     * Reads the data from a spreadsheet.
+     * Unfortunately the fastexcel library does currently only support xlsx.
+     *
+     * @throws IOException if the file cannot be read, in particular it'll be a {@link ZipException} if it's not a valid xlsx file, e.g.
+     *                     an .xls or .ods or other file.
+     */
+    protected Map<String, String> extractRulesFromXlsx(Resource resource, int sheetIndex, int startRow,
+                                                       int keyColumn, int valueColumn) throws IOException {
+        Map<String, String> rules = new LinkedHashMap<>();
+        try (InputStream inputStream = getAssetInputStream(resource);
+             ReadableWorkbook workbook = new ReadableWorkbook(inputStream)) {
+            Optional<Sheet> sheetOpt = workbook.getSheet(sheetIndex);
+            if (sheetOpt.isPresent()) {
+                Sheet sheet = sheetOpt.get();
+                try (Stream<Row> rows = sheet.openStream()) {
+                    rows.forEach(row -> {
+                        if (row.getRowNum() <= startRow) { // getRowNum() starts with 1
+                            return;
+                        }
+                        String key = row.getCellAsString(keyColumn).orElse(null);
+                        String value = row.getCellAsString(valueColumn).orElse(null);
+                        if (key != null && !key.isEmpty() && value != null && !value.isEmpty()) {
+                            rules.put(key, value);
+                        }
+                    });
+                }
+            } else {
+                throw new IllegalArgumentException("Sheet at index " + sheetIndex + " not found in " + resource.getPath());
+            }
+        }
+        if (rules.isEmpty()) {
+            throw new IllegalArgumentException("No rules found in " + resource.getPath());
+        }
+        LOG.info("Extracted {} rules from {}", rules.size(), resource.getPath());
         return rules;
     }
 
-    protected InputStream getAssetInputStream(Resource xlsResource)
-            throws NoSuchElementException {
+    /**
+     * Reads the data from a CSV file.
+     */
+    protected Map<String, String> extractRulesFromCsv(Resource resource, int sheetIndex, int startRow,
+                                                      int keyColumn, int valueColumn) throws IOException {
+        Csv csv = new Csv();
+        try (InputStream inputStream = getAssetInputStream(resource)) {
+            Iterator<String[]> data = csv.read(inputStream, "UTF-8");
+            for (int i = 0; i < startRow; i++) {
+                if (!data.hasNext()) {
+                    throw new IllegalArgumentException("Not enough rows in CSV file " + resource.getPath());
+                }
+                data.next();
+            }
+            Map<String, String> rules = new LinkedHashMap<>();
+            while (data.hasNext()) {
+                String[] row = data.next();
+                if (row.length > keyColumn && row.length > valueColumn) {
+                    String key = row[keyColumn];
+                    String value = row[valueColumn];
+                    if (key != null && !key.isEmpty() && value != null && !value.isEmpty()) {
+                        rules.put(key, value);
+                    }
+                }
+            }
+            return rules;
+        }
+    }
+
+    protected InputStream getAssetInputStream(Resource xlsResource) throws NoSuchElementException {
         return Optional.ofNullable(xlsResource.adaptTo(Asset.class))
                 .map(Asset::getOriginal)
                 .map(Rendition::getStream)
                 .orElseThrow(NoSuchElementException::new);
     }
-
 }
--- aem/core/src/test/java/com/composum/ai/aem/core/impl/autotranslate/TranslationRuleExtractorTest.java
+++ aem/core/src/test/java/com/composum/ai/aem/core/impl/autotranslate/TranslationRuleExtractorTest.java
@@ -1,25 +1,60 @@
 package com.composum.ai.aem.core.impl.autotranslate;
 
-import java.io.ByteArrayInputStream;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.apache.sling.api.resource.Resource;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import junit.framework.TestCase;
 
 public class TranslationRuleExtractorTest extends TestCase {
 
-    public void testExtractRules() throws IOException {
+    @Mock
+    private Resource xlsResource;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testExtractRulesFromXSLX() throws IOException {
+        TranslationRuleExtractor extractor = getTranslationRuleExtractor("translationtables/testtranslationtable.xlsx");
+        Map<String, String> rules1 = extractor.extractRules(xlsResource, 1, 1, "A", "B");
+        assertEquals("{crossroads=Kreuzung, traffic lights=Ampel}", String.valueOf(rules1));
+        assertEquals(2, rules1.size());
+
+        Map<String, String> rules2 = extractor.extractRules(xlsResource, 2, 3, "C", "B");
+        assertEquals("{Apple=Apfel, Egg=Ei}", String.valueOf(rules2));
+        assertEquals(2, rules2.size());
+    }
+
+    @Test
+    public void testExtractRulesFromCsv() throws IOException {
+        TranslationRuleExtractor extractor = getTranslationRuleExtractor("translationtables/testtranslationtable.csv");
+        Map<String, String> rules = extractor.extractRules(xlsResource, 1, 3, "C", "B");
+        assertEquals("{Apple=Apfel, Egg=Ei}", String.valueOf(rules));
+        assertEquals(2, rules.size());
+    }
+
+    private @NotNull TranslationRuleExtractor getTranslationRuleExtractor(String filename) {
+        when(xlsResource.getPath()).thenReturn(filename);
         TranslationRuleExtractor extractor = new TranslationRuleExtractor() {
             @Override
             protected InputStream getAssetInputStream(Resource xlsResource) throws NoSuchElementException {
-                return getClass().getClassLoader().getResourceAsStream("hpsx/test1.xlsx");
+                return getClass().getClassLoader().getResourceAsStream(filename);
             }
         };
-        Map<String, String> rules = extractor.extractRules(null, 0, 1, 0, 1);
-        assertEquals(2, rules.size());
+        return extractor;
     }
+
 }
