f2d98633ecf8c76b763fd0c2d8e0b7281866c23d
Dr. Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2025-03-04 09:56:57 +0100

    make scrolling to component robuster

--- aem/ui.frontend/src/main/webpack/site/editorExtensions.js
+++ aem/ui.frontend/src/main/webpack/site/editorExtensions.js
@@ -10,36 +10,46 @@ var retries;
 // e.g. http://localhost:5502/editor.html/content/gfps/com/en/about-us/going-forward.html#scrolltocomponent-content/container_copy_52774894/item_1510732628
 function maybeScrollToComponent(event, args) {
     console.log('maybeScrollToComponent', event.type, event.target, args);
-    if (Granite?.author?.OverlayWrapper?.$el) {
-        Coral.commons.ready(Granite.author.OverlayWrapper.$el, function () {
-                if (waitingTimeout) {
-                    clearTimeout(waitingTimeout);
-                }
-                retries = 3;
-                waitingTimeout = setTimeout(scrollToComponent, 2000);
-            }
-        );
-    }
+    Coral.commons.ready(document, function () {
+        if (waitingTimeout) {
+            clearTimeout(waitingTimeout);
+        }
+        retries = 40;
+        waitingTimeout = setTimeout(scrollToComponent, 500);
+    });
 }
 
 function scrollToComponent() {
-    const urlParams = new URL(window.location.href);
-    const anchor = urlParams.hash;
+    const url = new URL(window.location.href);
+    const anchor = url.hash;
+    url.hash = '';
+    const newUrl = url.href;
 
     if (anchor.startsWith('#scrolltocomponent-')) {
+        console.log('scrollToComponent', anchor);
         const path = Granite.author.page.path + '/jcr:content/' +
             anchor.replace('#scrolltocomponent-', '');
-        const element = Granite.author.OverlayWrapper.$el.find(`div[data-path="${path}"]`);
-        debugger;
-        if (element.size()) {
-            element[0].scrollIntoView({behavior: 'smooth', block: 'start'});
+        const element = Granite?.author?.OverlayWrapper?.$el?.find(`div.cq-Overlay[data-path="${path}"]`);
+        console.log('scrollToComponent', path, element.size(), element);
+        if (element && element.size()) {
+            setTimeout(() =>
+                    Coral.commons.ready(Granite.author.OverlayWrapper.$el, () =>
+                        element[0].scrollIntoView({behavior: 'smooth', block: 'start'})),
+                500
+            );
+            setTimeout(() => // sometimes that doesn't work - retry :-(
+                    Coral.commons.ready(Granite.author.OverlayWrapper.$el, () =>
+                        element[0].scrollIntoView({behavior: 'smooth', block: 'start'})),
+                2500
+            );
             // Remove the anchor from the URL
-            history.pushState({}, '', window.location.href.split('#')[0]);
+            history.pushState({}, '', newUrl);
         } else if (retries > 0) {
+            console.log('scrollToComponent retry', retries);
             retries--;
-            waitingTimeout = setTimeout(scrollToComponent, 4000);
+            waitingTimeout = setTimeout(scrollToComponent, 500);
         } else { // give up.
-            history.pushState({}, '', window.location.href.split('#')[0]);
+            history.pushState({}, '', newUrl);
         }
     }
 }
