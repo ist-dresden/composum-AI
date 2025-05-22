0d7e3b311c12dc463f7f9e01706775e4e60244f9
Ralf Wunsch <r.wunsch@ist-software.com>
2025-05-22 13:53:28 +0000

    Deploying to gh-pages from @ ist-dresden/composum-AI@4a52f8a371dbe8cbb009b8fb1a8a1fdea0f5c905 🚀

--- composum-ai-integration-composum/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.composum.ai.composum.bundle"},{"l":"com.composum.ai.composum.bundle.model"},{"l":"com.composum.ai.composum.bundle.service"}]
